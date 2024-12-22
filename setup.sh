49561ca63f3035a998b5201d0f5bd02fc35ffa0e
Darshan S M <147638293+darshanmalagimani@users.noreply.github.com>
2024-12-22 15:55:28 +0530

    Create setup.sh
    Installation and Initialization Setup Shell Script

---
+++ setup.sh
@@ -0,0 +1,39 @@
+# Kubernetes Cluster Installation using kubeadm
+Step 1 : Choosing a Container Runtime
+
+# Enabling IPV4 Packet Forwarding
+
+# To manually enable IPv4 packet forwarding:
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+# Verify that net.ipv4.ip_forward is set to 1 with execution of the following command:
+
+sysctl net.ipv4.ip_forward
+
+# Temporarily Turn Off Swap Using:
+sudo swapoff -a
+
+# Installing containerd runtime using apt package:
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Installing containerd 
+sudo apt-get install containerd.io
